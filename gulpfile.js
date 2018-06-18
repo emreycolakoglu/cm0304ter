cd1de8400bb30b0ebffe397980192ec6f9600822
Emre Yasin Colakoglu <emrinho@gmail.com>
2018-06-18 14:43:16 +0300

    typography progress

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var clean = require("gulp-clean");
+var copy = require("gulp-copy");
 
 gulp.task("sass", function() {
   return gulp
     .src("./src/**/*.scss")
     .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
     .pipe(gulp.dest("./dist/css"));
+});
+
+gulp.task("copy-fonts", function(){
+  return gulp.src(["./src/static/fonts/*"]).pipe(copy("./dist/static/fonts", { prefix: 10 }));
 });
 
 gulp.task("clean", function() {
@@ -17,4 +22,5 @@ gulp.task("sass:watch", function() {
   gulp.watch("./src/**/*.scss", ["sass"]);
 });
 
-gulp.task("watch", ["sass:watch"])
+gulp.task("watch", ["sass:watch"]);
+gulp.task("default", ["clean", "sass", "copy-fonts"]);
